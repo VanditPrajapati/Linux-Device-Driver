54fb1090d6eac5f53205d90df876de2e8f45698f
VanditPrajapati <Vandit.Prajapati@einfochips.com>
2022-06-06 15:03:25 +0530

    Character driver with procfs entry but not able to unload the module

--- char_driver.c
+++ char_driver.c
@@ -9,6 +9,7 @@
 #include<linux/version.h>
 #include<linux/slab.h>
 #include<linux/ioctl.h>
+#include<linux/proc_fs.h>
 
 #define WR_DATA _IOW('a','a',char *)
 #define RD_DATA _IOR('a','b',char *)
@@ -25,6 +26,11 @@ static struct cdev char_dev;
 static struct class *cl;
 
 static struct kobject *kobj_ref;
+
+static struct proc_dir_entry *parent;
+
+static char array[20] = "Vandit Prajapati\n";
+static int len = 1;
 
 uint8_t *kernel_buffer;
 size_t memsize = 1024;
@@ -130,6 +136,46 @@ static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr, co
 	return count;
 }
 
+static int open_proc(struct inode *inode, struct file *file)
+{
+	printk(KERN_INFO "proc file opened\n");
+	return 0;
+}
+
+static int release_proc(struct inode *inode, struct file *file)
+{
+	printk(KERN_INFO "proc file closed\n");
+	return 0;
+}
+
+static ssize_t read_proc(struct file *file, char __user *buf, size_t count, loff_t *offset)
+{
+	printk(KERN_INFO "proc file read..\n");
+	if(len)
+	{
+		len = 0;
+	}
+	else
+	{
+		len = 1;
+		return 0;
+	}
+
+	if(copy_to_user(buf,array,20))
+	{
+		printk(KERN_INFO "Data read error\n");
+	}
+
+	return count;
+}
+
+static ssize_t write_proc(struct file *file, const char *buf, size_t count, loff_t *offset)
+{
+	printk(KERN_INFO "Data Writing\n");
+	copy_from_user(array,buf,count);
+	return count;
+}
+
 static struct file_operations char_fops = {
 	.owner = THIS_MODULE,
 	.open = char_open,
@@ -137,6 +183,13 @@ static struct file_operations char_fops = {
 	.unlocked_ioctl = char_ioctl,
 	.read = char_read,
 	.write = char_write
+};
+
+static struct file_operations proc_fops = {
+	.open = open_proc,
+	.release = release_proc,
+	.read = read_proc,
+	.write = write_proc
 };
 
 static struct kobj_attribute chardriver_attr = __ATTR(value, 0660, sysfs_show, sysfs_store);
@@ -170,6 +223,16 @@ static int __init function_init(void)
 		printk(KERN_INFO "Device cannot be created\n");
 		goto r_device;
 	}
+ 
+	parent = proc_mkdir("chardriver", NULL);
+	
+	if(parent == NULL)
+	{
+		printk(KERN_INFO "Error creating proc entry\n");
+		goto r_device;
+	}
+
+	proc_create("chardriver_proc", 0666, parent, &proc_fops);
 
 	kobj_ref = kobject_create_and_add("chardriver_sysfs", NULL);
 
@@ -198,6 +261,7 @@ r_class:
 
 static void __exit function_exit(void)
 {
+	remove_proc_entry("chardriver/chardriver_proc",parent);
 	kobject_put(kobj_ref);
 	sysfs_remove_file(NULL, &chardriver_attr.attr);
 	device_destroy(cl, first);
